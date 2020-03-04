37a8a9c173f2038db13ad24a2996e3c46b5785fb
kostenko <serg.kostenko@gmail.com>
2020-03-04 13:51:25 +0200

    upd

---
+++ blog/2020/03/firebase-push-microprofile-rest-client/init.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBb44LhjzycF3j9BYLmv98ykKsnCAFM9EM",
+  authDomain: "phonic-vortex-206716.firebaseapp.com",
+  databaseURL: "https://phonic-vortex-206716.firebaseio.com",
+  projectId: "phonic-vortex-206716",
+  storageBucket: "phonic-vortex-206716.appspot.com",
+  messagingSenderId: "1019514751044",
+  appId: "1:1019514751044:web:3e8d49fdf1f939cf761411",
+  measurementId: "G-02PRR90VNE"
+};
+firebase.initializeApp(config);
